254ca7948459a0ca207fa4f2e09c95efe9139515
Hammad Afzaal <madirana530@gmail.com>
2024-12-17 16:37:41 +0500

    working at header

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -1,6 +1,19 @@
 const Header = () => {
     return (
-        <div>Header</div>
+        <>
+            <div className="tw-flex tw-flex-col tw-bg-g">
+                <div className="tw-flex tw-gap-1">
+                    <div className="tw-bg-green-600 tw-text-lg tw-font-extrabold tw-text-white tw-p-1">PROPERTY</div>
+                    {/* Icon */}
+                </div>
+                <span className="tw-text-lg tw-font-bold tw-text-white">
+                    CHECK
+                </span>
+            </div>
+
+        </>
+
+
     )
 }
 
