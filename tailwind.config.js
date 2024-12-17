e78a087f2e6501adcd1130302fe071c4b79577ee
Ayhab Tariq <96698058+Ayhab-Tariq@users.noreply.github.com>
2024-12-17 16:40:26 +0500

    setting up colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  prefix: "tw-",
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#038137", // Default primary color (e.g., blue-700)
+          light: "", // Lighter shade
+          dark: "", // Darker shade
+        },
+        secondary: {
+          DEFAULT: "#1d1d1b", // Default secondary color (e.g., purple-600)
+          light: "", // Lighter shade
+          dark: "", // Darker shade
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
