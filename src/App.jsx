1588e33368cd917bda5cf28e1dfa7666e6652437
Hammad Afzaal <madirana530@gmail.com>
2024-12-17 18:55:10 +0500

    working at the black book

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './variables.css'
 import Header from "./layouts/Header"
+import BlackBook from "./pages/BlackBook"
 
 function App() {
 
 
   return (
     <>
+
       <Header />
+      <BlackBook />
       <h1 className='text-green-400'>hello</h1>
     </>
   )
---
+++ src/pages/BlackBook.jsx
@@ -0,0 +1,11 @@
+import Card from "../components/Card"
+
+const BlackBook = () => {
+    return (
+        <div className="tw-flex tw-self-start tw-justify-center">
+            <Card />
+        </div>
+    )
+}
+
+export default BlackBook
